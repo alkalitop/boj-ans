9486901e2cba35b666788d57afbe17cb2c83710b
Sanha <47966951+sweetcorn1229@users.noreply.github.com>
2022-03-11 22:18:56 +0900

    Create 2839.py

---
+++ 2839/2839.py
@@ -0,0 +1,11 @@
+N = int(input())
+sol = [0, 3, 5, 6, 8, 9]
+val = [0, 3, 4, 1, 4, 1, 2, 5, 2, 3]
+a = N%10
+if a in sol:
+    print((N//10)*2+val[a])
+    else:
+        if N < 10:
+	    print(-1)
+	else:
+	    print(((N//10)-1)*2+val[a])
