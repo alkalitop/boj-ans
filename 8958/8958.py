6544145f21e3bc1f7fc1239443af7c046e059317
Sanha <47966951+sweetcorn1229@users.noreply.github.com>
2022-03-13 13:06:41 +0900

    Create 8958.py

---
+++ 8958/8958.py
@@ -0,0 +1,12 @@
+tc = int(input())
+for i in range(0, tc):
+    q = input()
+    score = 0
+    depth = 1
+    for j in range(0, len(q)):
+        if q[j] == 'O':
+	    score += depth
+	    depth += 1
+	else:
+	    depth = 1
+    print(score)
