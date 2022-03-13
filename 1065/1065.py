e5e929ada51aa0080375f456bd7d32eaea7f3850
Sanha <47966951+sweetcorn1229@users.noreply.github.com>
2022-03-13 21:41:22 +0900

    Create 1065.py

---
+++ 1065/1065.py
@@ -0,0 +1,20 @@
+N = input()
+c = 0
+
+def func (n):
+    d = []
+    n = str(n)
+    for i in range(0, len(n)-1):
+        d.append(int(n[i+1]) - int(n[i]));
+    if len(set(d)) == 1:
+        return 1
+    else:
+        return 0
+   
+for n in range(1, int(N)+1):
+    if n < 10:
+        c += 1
+    else: 
+	c += func(n)  
+ 
+print(c)
