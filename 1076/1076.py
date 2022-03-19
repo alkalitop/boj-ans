9067f2f18ef4fdb66f7f8e0f103cc29e9e23ea2b
Sanha <47966951+sweetcorn1229@users.noreply.github.com>
2022-03-20 01:37:19 +0900

    Create 1076.py

---
+++ 1076/1076.py
@@ -0,0 +1,16 @@
+r = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+x = input()
+y = input()
+z = input()
+print((r[x]*10+r[y])*(10**r[z]))
