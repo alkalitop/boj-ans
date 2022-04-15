557e62d8d27c7723f11253d3c0b8198bc0568233
Sanha (AFlat) <47966951+sanha1229@users.noreply.github.com>
2022-04-15 11:50:09 +0900

    Create stack.py

---
+++ sample/python/stack.py
@@ -0,0 +1,19 @@
+class stack:
+    def __init__ (self):
+        self.els = []    
+    def push (el):
+        self.els.append(el)
+        return el
+    def top ():
+        if self.size() == 0:
+            return None
+        return self.els[0]
+    def pop ():
+        value = self.top()
+        if value != None:
+            delete self.els[0]
+        return value        
+    def size ():
+        return len(self.els)
+    def is_empty ():
+        return self.size() == 0
