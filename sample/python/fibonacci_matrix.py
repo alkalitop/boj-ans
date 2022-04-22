5fd6f58c4c6299519053c8a405bb1ab7af16a37a
Sanha (AFlat) <47966951+sanha1229@users.noreply.github.com>
2022-04-22 12:00:58 +0900

    Update fibonacci_matrix.py

--- sample/python/fibonacci_matrix.py
+++ sample/python/fibonacci_matrix.py
@@ -2,7 +2,23 @@ def matrix_mul (A, B):
     return ((A[0][0]*B[0][0]+A[0][1]*B[1][0], A[0][0]*B[0][1]+A[0][1]*B[1][1]),
             (A[1][0]*B[0][0]+A[1][1]*B[1][0], A[1][0]*B[0][1]+A[1][1]*B[1][1]))
     
-def matrix_pow (A, n):
-    
+def matrix_pow (A, n, p):
+    if n == 0:
+        return A
+    t = matrix_pow(A, n//2, p)
+    if n % 2 == 1:
+        A_ = matrix_mul(matrix_mul(t, t), A)
+        A_[0][0] %= p
+        A_[0][1] %= p
+        A_[1][0] %= p
+        A_[1][1] %= p
+        return A_
+    else:
+        A_ = matrix_mul(t, t)
+        A_[0][0] %= p
+        A_[0][1] %= p
+        A_[1][0] %= p
+        A_[1][1] %= p
+        return A_
     
     
