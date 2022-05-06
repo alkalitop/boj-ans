0dba17dd2b54dfc9fe442eb577e2e7498877680b
내림가장조 <47966951+sanha1229@users.noreply.github.com>
2022-05-07 03:24:34 +0900

    Create 1541.py

---
+++ 1541/1541.py
@@ -0,0 +1,10 @@
+import re
+
+s = list(input())
+ops = 0
+size = len(s)
+for i in range(size):
+    if s[i] == '-' and not ops: ops = 1
+    if s[i] == '+' and ops: s[i] = '-'
+ex = re.sub('\-0+', '-', re.sub('^0+', '', ''.join(s)))
+print(eval(ex))
