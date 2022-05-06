dffd80cf862df9f028f9649982f4f8c52f2afe80
내림가장조 <47966951+sanha1229@users.noreply.github.com>
2022-05-06 23:23:09 +0900

    Update 11047.py

--- 11047/11047.py
+++ 11047/11047.py
@@ -5,13 +5,13 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 coin = [0]*n
 for i in range(n):
-	coin[i] = int(input())
+    coin[i] = int(input())
 
 cnt = 0
 rest = k
 while rest > 0:
-	L = rest if rest in coin else coin[bisect_left(coin, rest)-1]
-	cnt += rest // L
-	rest %= L
+    L = rest if rest in coin else coin[bisect_left(coin, rest)-1]
+    cnt += rest // L
+    rest %= L
 	
 print(cnt)
