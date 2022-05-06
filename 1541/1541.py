686560e4b5983feab1bd1376b68ebb768f0ee932
내림가장조 <47966951+sanha1229@users.noreply.github.com>
2022-05-07 03:27:01 +0900

    Update 1541.py

--- 1541/1541.py
+++ 1541/1541.py
@@ -6,5 +6,5 @@ size = len(s)
 for i in range(size):
     if s[i] == '-' and not ops: ops = 1
     if s[i] == '+' and ops: s[i] = '-'
-ex = re.sub('\-0+', '-', re.sub('^0+', '', ''.join(s)))
+ex = re.sub('\+0+', '+', re.sub('\-0+', '-', re.sub('^0+', '', ''.join(s))))
 print(eval(ex))
