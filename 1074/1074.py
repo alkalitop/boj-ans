7ae83b3598c192cd9f118c6dd3518f3efb0d7f37
내림가장조 <47966951+sanha1229@users.noreply.github.com>
2022-05-06 20:33:24 +0900

    Create 1074.py

---
+++ 1074/1074.py
@@ -0,0 +1,20 @@
+move = ((0, 0), (-1, 0), (0, -1), (-1, -1)) # (x, y)
+
+def Z (n, x, y):
+	if n == 0:
+		return 0
+	m = n >> 1
+	q = 0
+	if y < m:
+		if x >= m:
+			q = 1
+	else:
+		if x < m:
+			q = 2
+		else:
+			q = 3
+	t = Z(m, x+move[q][0]*m, y+move[q][1]*m) + q*(m**2)
+	return t
+	
+N, r, c = map(int, input().split())
+print(Z(1 << N, c, r))
