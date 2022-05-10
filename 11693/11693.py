4cb01a0cf4957b93d25496a9b163a4e7d7489d37
Flachan <47966951+sanha1229@users.noreply.github.com>
2022-05-10 21:47:41 +0900

    Update 11693.py

--- 11693/11693.py
+++ 11693/11693.py
@@ -12,16 +12,16 @@ def dcpow (a, n, p):
         return t*t % p
 
 for i in range(2, n):
-	if n == 1 or i*i > n: break
-	c = 0
-	while n % i == 0:
-		c += 1
-		n //= i
-	if c == 0: continue
-	res *= ((dcpow(i % p, c*m + 1, p) - 1) % p) * dcpow((i - 1) % p, p - 2, p)
-	res %= p
+    if n == 1 or i*i > n: break
+    c = 0
+    while n % i == 0:
+        c += 1
+        n //= i
+    if c == 0: continue
+    res *= ((dcpow(i % p, c*m + 1, p) - 1) % p) * dcpow((i - 1) % p, p - 2, p)
+    res %= p
 
 if n > 1:
-	res *= ((dcpow(n % p, m + 1, p) - 1) % p) * dcpow((n - 1) % p, p - 2, p)
+    res *= ((dcpow(n % p, m + 1, p) - 1) % p) * dcpow((n - 1) % p, p - 2, p)
 
 print(res % p)
