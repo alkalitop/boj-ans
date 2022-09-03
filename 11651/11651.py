3b4909226836c3e54f5f82b9b9ddf00267a109d4
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-09-03 09:25:10 +0900

    Update 11651.py

--- 11651/11651.py
+++ 11651/11651.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 arr = [0]*N
-for i in range(0, N):
+for i in range(N):
     x, y = map(int, input().split())
     arr[i] = (y, x)
 arr.sort()
-for i in range(0, N):
+for i in range(N):
     print(arr[i][1], arr[i][0])
