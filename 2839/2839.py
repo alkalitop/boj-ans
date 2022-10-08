ccb9b2fc6e32a78e034c7c3eaa1d5ae28d947e60
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-10-08 11:08:08 +0900

    Update 2839.py

--- 2839/2839.py
+++ 2839/2839.py
@@ -4,8 +4,8 @@ val = [0, 3, 4, 1, 4, 1, 2, 5, 2, 3]
 a = N%10
 if a in sol:
     print((N//10)*2+val[a])
+else:
+    if N < 10:
+        print(-1)
     else:
-        if N < 10:
-	    print(-1)
-	else:
-	    print(((N//10)-1)*2+val[a])
+        print(((N//10)-1)*2+val[a])
