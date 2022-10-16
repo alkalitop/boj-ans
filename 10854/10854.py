eb6b8e5e08406dfcb8d581ec6fa1e9dcd6c3e501
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-10-16 10:30:29 +0900

    Create 10854.py

---
+++ 10854/10854.py
@@ -0,0 +1,158 @@
+from math import gcd, log2, floor
+from random import random
+
+def mrpt_seg (n, a):
+	d = n-1
+	r = 0
+	while d % 2 == 0:
+		d >>= 1
+		r += 1
+	t = pow(a, d, n)
+	if t == 1 or t == n-1:
+		return 1
+	for i in range(r-1):
+		t = pow(t, 2, n)
+		if t == n-1:
+			return 1
+	return 0
+	
+def mrpt (n):
+	tmp = 0
+	prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+	if n in prime:
+		return 1
+	if n % 2 == 0:
+		return 0
+	for a in prime:
+		if not mrpt_seg(n, a):
+			return 0
+	return 1
+
+def numadd (a, b, p = 0):
+	if p == 0:
+		return a+b
+	return (a+b)%p
+
+def nummul (a, b, p = 0):
+	if p == 0:
+		return a*b
+	return a*b%p
+
+def rho_seg (n, x):
+	if n == 1:
+		return 0
+	if n % 2 == 0:
+		return 2
+	if mrpt(n):
+		return n
+		
+	y = x
+	d = 1
+	
+	while d == 1:
+		x = numadd(nummul(x, x, n), 1, n)
+		y = numadd(nummul(y, y, n), 1, n)
+		y = numadd(nummul(y, y, n), 1, n)
+		
+		d = gcd(abs(x-y), n)
+		
+	if d == n:
+		return rho_seg2(d, x)
+	else:
+		if mrpt(d):
+			return d
+		else:
+			return rho_seg(d, x)
+			
+def rho_seg2 (n, x):
+	if n == 1:
+		return 0
+	if n % 2 == 0:
+		return 2
+	if mrpt(n):
+		return n
+		
+	y = x
+	d = 1
+	
+	while d == 1:
+		x = numadd(nummul(x, x, n), -1, n)
+		y = numadd(nummul(y, y, n), -1, n)
+		y = numadd(nummul(y, y, n), -1, n)
+		
+		d = gcd(abs(x-y), n)
+		
+	if d == n:
+		return rho_seg3(d, x)
+	else:
+		if mrpt(d):
+			return d
+		else:
+			return rho_seg2(d, x)
+			
+def rho_seg3 (n, x):
+	if n == 1:
+		return 1
+	if n % 2 == 0:
+		return 2
+	if mrpt(n):
+		return n
+		
+	y = x
+	d = 1
+	
+	while d == 1:
+		x = numadd(nummul(x, x, n), 2, n)
+		y = numadd(nummul(y, y, n), 2, n)
+		y = numadd(nummul(y, y, n), 2, n)
+		
+		d = gcd(abs(x-y), n)
+		
+	if d == n:
+		return 0
+	else:
+		if mrpt(d):
+			return d
+		else:
+			return rho_seg3(d, x)
+
+def plr (n):
+	x = 2
+	res = []
+	i = 0
+	while n > 1:
+		p = rho_seg(n, x)
+		if p:
+			res.append(p)
+			i += 1
+			n //= p
+			if n == 1:
+				return res
+			if mrpt(n):
+				res.append(n)
+				return res
+		else:
+			x = floor((random()*log2(n+1)))+2
+	return res
+
+n = int(input())
+
+if n == 1:
+	print(1)
+else:
+	factor = sorted(plr(n))
+
+	tmp1 = 1
+	tmp2 = 0
+	r = 1
+
+	for i in factor:
+		if i != tmp1:
+			r *= tmp2+1
+			tmp1 = i
+			tmp2 = 1
+		else:
+			tmp2 += 1
+
+	r *= tmp2+1
+	print(r)
