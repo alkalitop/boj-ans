5a13920617743e02df10d73c8f43f8b9795c897b
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-10-29 10:45:20 +0900

    Update 8958.py

--- 8958/8958.py
+++ 8958/8958.py
@@ -3,7 +3,7 @@ for i in range(0, tc):
     q = input()
     score = 0
     depth = 1
-    for j in range(0, len(q)):
+    for j in range(len(q)):
         if q[j] == 'O':
 	    score += depth
 	    depth += 1
