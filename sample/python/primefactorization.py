40299ac5a78ab08a648e62db9ec078f88153cf83
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-10-29 12:54:37 +0900

    Update and rename primefactorization.py to pollard_rho.py

--- sample/python/primefactorization.py
+++
@@ -1,9 +0,0 @@
-def pf (n):
-    factor = {}
-    for i in range(2, n//2+1):
-        while n % i == 0:
-            if not i in factor: factor[i] = 0
-            factor[i] += 1
-            n //= i
-        if n > 1: factor[n] = 1
-    return factor
