ede1cf602b86fe20a4c5cd29fe4136683ead48fc
AFlat <47966951+sanha1229@users.noreply.github.com>
2022-11-18 10:13:34 +0900

    Update 4153.py

--- 4153/4153.py
+++ 4153/4153.py
@@ -3,7 +3,7 @@ h2 = lambda x,y: x**2+y**2
 while a != 0:
     if h2(a,b) == c**2 or h2(b,c) == a**2 or h2(c,a) == b**2:
 	print('right')
-	a, b, c = map(int, input().split(' '))
+	a, b, c = map(int, input().split())
     else:
 	print('wrong')
-	a, b, c = map(int, input().split(' '))
+	a, b, c = map(int, input().split())
