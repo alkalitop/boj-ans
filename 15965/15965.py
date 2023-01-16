e73d27b6506f21601e4a6a08f5ecbf819fc8d5a1
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-01-16 11:47:27 +0900

    Create 15965.py

---
+++ 15965/15965.py
@@ -0,0 +1,17 @@
+def eratosthenes(M):
+    ret = [1]*(M+1)
+    ret[0], ret[1] = (0, 0)
+    p = 2
+    while p in range(int(M**0.5)+1):
+        if ret[p]:
+            ret[p] = p
+            i = 2
+            while p*i <= M:
+                ret[p*i] = 0
+                i += 1
+        p += 1
+    return list(filter(lambda i: ret[i], range(M+1)))
+    
+primes = eratosthenes(7368787)
+
+print(primes[int(input())-1])
