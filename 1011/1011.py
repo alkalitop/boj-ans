98652b6b75bde47c7695b3745e93f43de5a75fcb
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-01-18 14:03:50 +0900

    Update 1011.py

--- 1011/1011.py
+++ 1011/1011.py
@@ -1,8 +1,5 @@
-import sys
 import math
-input = sys.stdin.readline
-f = lambda n: math.floor(math.sqrt(4*n+1))
 
 for i in range(int(input())):
     a, b = map(int, input().split())
-    print(f(b-a-1))
+    print(math.sqrt(((b-a-1)<<2)+1))
