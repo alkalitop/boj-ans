1f7cc685220ae7e446256c30452c01ad88a2f419
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-02-01 12:06:26 +0900

    Update 3783.py

--- 3783/3783.py
+++ 3783/3783.py
@@ -3,5 +3,5 @@ from decimal import *
 getcontext().prec = 1000
 t = Decimal('1')/Decimal('3')
 for i in range(int(input())):
-    a = round(Decimal(Decimal(input().rstrip()+'.'+'0'*10)**t),500)
+    a = round(Decimal(Decimal(input().rstrip()+'.'+'0'*10)**t),250)
     print(Decimal(a).quantize(Decimal('.'+('0'*9)+'1'), rounding=ROUND_DOWN))
