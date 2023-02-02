92f64b143ba1c555a0f4a125f99d249494d9273f
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-02-02 19:21:44 +0900

    Create 23832.py

---
+++ 23832/23832.py
@@ -0,0 +1,38 @@
+def eratosthenes(M):
+    ret = [1]*(M+1)
+    ret[0], ret[1] = (0, 0)
+    p = 2
+    while p in range(int(M**0.5)+1):
+        if ret[p]:
+            ret[p] = 1
+            i = 2
+            while p*i <= M:
+                ret[p*i] = 0
+                i += 1
+        p += 1
+    return list(filter(lambda i: ret[i], range(M+1)))
+
+prime = eratosthenes(5*(10**4))
+
+cnt = 0
+for n in range(2, int(input())+1):
+    res = n
+    factor = []
+
+    for p in prime:
+        if n % p == 0:
+            factor.append(p)
+            while n % p == 0:
+                n //= p
+            
+    if n > 1:
+        factor.append(n)
+    
+    for f in factor:
+        res *= f-1
+        res //= f
+        
+    cnt += res
+
+print(cnt)
+
