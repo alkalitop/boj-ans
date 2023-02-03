3b4b42a75f9aa69300594b3cb5ac529d017b9e9b
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-02-03 21:38:43 +0900

    Update sum_segmenttree.py

--- sample/python/sum_segmenttree.py
+++ sample/python/sum_segmenttree.py
@@ -11,6 +11,19 @@ def segmentify(tree, index, merge, arr):
     tree[idx] = merge(a, b)
     return tree[idx]
 
+def segquery(tree, index, merge, arr=0, query=0, default):
+    start, end, idx = index # 리스트 시작 인덱스, 리스트 끝 인덱스, 트리 인덱스
+    L, R = interval # 합 시작, 합 끝
+    
+    if R < start or L > end:
+        return outofrange
+    elif L <= start and R >= end:
+        return tree[idx]
+    else:
+        mid = (start+end)//2
+        a = segsum(tree, (start, mid, idx<<1), (L, R))
+        b = segsum(tree, (mid+1, end, (idx<<1)+1), (L, R))
+
 class SumSegmentTree:
 
     def __init__(self, arr):
@@ -21,27 +34,27 @@ class SumSegmentTree:
         segmentify(tree=self.tree, index=(0,(self.size>>2)-1,1), merge=self.merge, arr=self.arr)
         return self.tree
     
-def segsum(tree, index, interval, merge):
-    start, end, idx = index # 리스트 시작 인덱스, 리스트 끝 인덱스, 트리 인덱스
-    L, R = interval # 합 시작, 합 끝
+    def sum(self, L, R, index=(0,(self.size>>2)-1,1)):
+        start, end, idx = index # 리스트 시작 인덱스, 리스트 끝 인덱스, 트리 인덱스
     
-    if R < start or L > end:
-        return 0
-    elif L <= start and R >= end:
-        return tree[idx]
-    else:
-        mid = (start+end)//2
-        a = segsum(tree, (start, mid, idx<<1), (L, R))
-        b = segsum(tree, (mid+1, end, (idx<<1)+1), (L, R))
+        if R < start or L > end:
+            return 0
+        elif L <= start and R >= end:
+            return self.tree[idx]
+        else:
+            mid = (start+end)//2
+            a = self.sum(self.tree, L, R, (start, mid, idx<<1))
+            b = self.sum(self.tree, L, R, (mid+1, end, (idx<<1)+1))
+            return self.merge(a, b)
         
-def segupdate(tree, index, target, val):
-    start, end, idx = index # 리스트 시작 인덱스, 리스트 끝 인덱스, 트리 인덱스
+    def update(self, target, value, index=(0,(self.size>>2)-1,1)):
+        start, end, idx = index # 리스트 시작 인덱스, 리스트 끝 인덱스, 트리 인덱스
     
-    if target < start or target > end: return
+        if target < start or target > end: return
     
-    tree[idx] += val
-    if start == end: return
+        tree[idx] += val
+        if start == end: return
     
-    mid = (start+end)//2
-    update(tree, (start, mid, idx<<1), target, val)
-    update(tree, (mid+1, end, (idx<<1)+1), target, val)
+        mid = (start+end)//2
+        self.update(tree, (start, mid, idx<<1), target, val)
+        self.update(tree, (mid+1, end, (idx<<1)+1), target, val)
