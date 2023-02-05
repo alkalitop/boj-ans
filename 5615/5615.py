4722a70e40d1e565d1a7af57f2eab0b07bfad3cc
AFlat <47966951+aflatlsh@users.noreply.github.com>
2023-02-05 09:25:48 +0900

    Update 5615.py

--- 5615/5615.py
+++ 5615/5615.py
@@ -1,17 +1,29 @@
-def miller_rabin (n, a):
-	d = n-1
-	r = 0
-	while d % 2 == 0:
-		d >>= 1
-		r += 1
-	t = pow(a, d, n)
-	if t == 1 or t == n-1:
-		return 1
-	for i in range(r-1):
-		t = pow(t, 2, n)
-		if t == n-1:
-			return 1
-	return 0
+def mrpt_seg (n, a):
+    d = n-1
+    r = 0
+    while d % 2 == 0:
+        d >>= 1
+        r += 1
+    t = pow(a, d, n)
+    if t == 1 or t == n-1:
+        return 1
+    for i in range(r-1):
+        t = pow(t, 2, n)
+        if t == n-1:
+            return 1
+    return 0
+	
+def mrpt (n):
+    tmp = 0
+    prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    if n in prime:
+        return 1
+    if n % 2 == 0:
+        return 0
+    for a in prime:
+        if not mrpt_seg(n, a):
+            return 0
+    return 1
 	
 import sys
 input = sys.stdin.readline
@@ -19,12 +31,8 @@ input = sys.stdin.readline
 cnt = 0
 
 for i in range(int(input())):
-	tmp = 0
-	s = int(input())
-	for a in [2, 3, 5, 7, 11]:
-		if miller_rabin(2*s+1, a):
-			tmp += 1
-		else:
-			break
-	cnt += tmp // 5
+    tmp = 0
+    s = int(input())
+    cnt += mrpt(2*s+1)
 	
+print(cnt)
