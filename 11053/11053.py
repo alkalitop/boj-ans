d4be4a91fcbf2dcf81cc9c5eb9c7bc60a9415697
Neulbin Lim <47966951+aflatlsh@users.noreply.github.com>
2023-06-11 19:57:55 +0900

    Create 11053.py

---
+++ 11053/11053.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+A = list(map(int, input().split()))
+dp = [0]*n
+
+for i in range(n):
+    dp[i] = 1
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+                
+print(max(dp))
