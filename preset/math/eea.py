7659c5bab55cb557cec44986792109499cbe7c7a
Neulbin Lim <47966951+aflatlsh@users.noreply.github.com>
2023-10-19 20:50:17 +0900

    Update eea.py

--- preset/math/eea.py
+++ preset/math/eea.py
@@ -2,10 +2,10 @@ def eea(n, m):
     r1, r2 = m, n
     t2 = -~(t1 := 0)
     while r2:
-        q = r1 // r2
-        r = r1 - q * r2
+        s = r1 // r2
+        r = r1 - s * r2
         r1, r2 = r2, r
-        t = t1 - q * t2
+        t = t1 - s * t2
         t1, t2 = t2, t
 
     if r1 != 1:
