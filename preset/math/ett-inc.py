33e1583bbbeba333ee2f3d152037d295937303ae
Neulbin Lim <47966951+aflatlsh@users.noreply.github.com>
2023-11-28 10:45:44 +0900

    Create ett-inc.py

---
+++ preset/math/ett-inc.py
@@ -0,0 +1,13 @@
+def ettinc(M):
+    ret = [1]*(M+1)
+    ret[0], ret[1] = (0, 0)
+    p = 2
+    while p in range(int(M**0.5)+1):
+        if ret[p]:
+            ret[p] = 1
+            i = 2
+            while p*i <= M:
+                ret[p*i] = 0
+                i += 1
+        p += 1
+    return ret
