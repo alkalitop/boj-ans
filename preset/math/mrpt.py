46d5bc842f63d3162681de5b5751c2258d24a8ad
Neulbin Lim <47966951+aflatlsh@users.noreply.github.com>
2023-11-28 10:48:13 +0900

    Create mrpt.py

---
+++ preset/math/mrpt.py
@@ -0,0 +1,28 @@
+from math import gcd
+
+def mrpt_seg (n, a):
+    d = ~-n
+    r = 0
+    while -~n & 1:
+        d >>= 1
+        r += 1
+    t = pow(a, d, n)
+    if t == 1 or t == ~-n:
+        return 1
+    for i in range(~-r):
+        t = pow(t, 2, n)
+        if t == ~-n:
+            return 1
+    return 0
+	
+def mrpt (n):
+    tmp = 0
+    prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    if n in prime:
+        return 1
+    if -~n & 1:
+        return 0
+    for a in prime:
+        if not mrpt_seg(n, a):
+            return 0
+    return 1
